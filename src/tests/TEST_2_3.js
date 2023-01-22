2beaa25efb0ceb285cefb551b4182ee41a653284
combsbt <combsbt@vcu.edu>
2023-01-22 18:54:02 -0500

    save

--- src/tests/TEST_2_3.js
+++ src/tests/TEST_2_3.js
@@ -5,9 +5,11 @@ export default function TEST_2_3() {
   const [file, setFile] = useState(null);
 
   function handleLoad(){
-    let gcdImg = gcd(document.getElementById("image").offsetWidth,document.getElementById("image").offsetHeight);
-    if(document.getElementById("image") && ratio !== [document.getElementById("image").offsetWidth/gcdImg,document.getElementById("image").offsetHeight/gcdImg] ){
-    setRatio([document.getElementById("image").offsetWidth/gcdImg,document.getElementById("image").offsetHeight/gcdImg])}
+    let width = document.getElementById("image").naturalWidth;
+    let height = document.getElementById("image").naturalHeight;
+    let gcdImg = gcd(width,height);
+    if(ratio !== [width/gcdImg,height/gcdImg] ){
+    setRatio([width/gcdImg,height/gcdImg])}
   }
 
   function gcd(a, b) {
@@ -21,12 +23,13 @@ export default function TEST_2_3() {
   return (
     <div>
       <h2>Image size ratio</h2>
-      <input type="file" data-test="file-input" onChange={(e)=>{console.log(e.target.files[0]);
-        setFile(URL.createObjectURL(e.target.files[0]))}}/>
+      <input type="file" data-test="file-input" onChange={(e)=>{
+        setFile(URL.createObjectURL(e.target.files[0]))
+      }}/>
       <div className="result" data-test="result">
         {ratio && ratio[0]+"x"+ratio[1]}
-        <img id="image" src={file && file} onLoad={ handleLoad }/>
       </div>
+      <img id="image" src={file && file} onLoad={ handleLoad }/>
     </div>
   );
 }
