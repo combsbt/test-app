56048a4e037546023dc22e5e96cd24497c1cea74
combsbt <combsbt@vcu.edu>
2023-01-23 21:11:56 -0500

    save

--- src/tests/TEST_2_4.js
+++ src/tests/TEST_2_4.js
@@ -5,8 +5,9 @@ import './styles.css';
 export default function TEST_2_4() {
 
   const [toggles, setToggles] = useState([null,0,0,0,0,0,0,0,0,0,0,0])
-  let children = [null, [3,4], null, [8,9,10], null, null, null, null, null, null, null]
-  let parents = [null, null, null, 1, 1, null, null, null, 3, 3, 3]
+  let children = [null, [3,4], [5,6,7], [8,9,10], [11,12,13], null, null, null, null, null, null, [14,15,16], null, null, null, null, null]
+  let parents = [null, null, null, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 11, 11, 11]
+  let testarr = arrayRtn();
 
   function toggleBtn(catNum){
     let newToggles = [];
@@ -39,8 +40,66 @@ export default function TEST_2_4() {
     }
   }
 
+  console.log(categories)
+
+  function arrayRtn(){
+    return [
+    <div key="blah">
+      <ul className="root" data-test="root">
+      {categories.map((cat)=>{return(
+        <li key={"category-"+cat.id.toString()} data-test={"category-"+cat.id.toString()}>
+          <button id={cat.id.toString()} data-test="toggle-btn" className="toggle" onClick={()=>{toggleBtn(cat.id)}}>
+            {toggles[cat.id]?"+":"-"}
+          </button>
+          {cat.name}
+          {cat.children &&
+            <ul>
+            {cat.children.map((child)=>{return(
+              <li hidden={toggles[parents[child.id]]} data-test={"category-"+child.id} key={"category-"+child.id}>
+              {child.children &&
+              <button id={child.id} data-test="toggle-btn" className="toggle" onClick={()=>{toggleBtn(child.id)}}>
+                {toggles[child.id]?"+":"-"}
+              </button>
+              }
+              {child.name}
+              {child.children && 
+                child.children.map((child2)=>{return(
+                  <li hidden={toggles[parents[child2.id]]} data-test={"category-"+child2.id} key={"category-"+child2.id}>
+                  {child2.children &&
+                  <button id={child2.id} data-test="toggle-btn" className="toggle" onClick={()=>{toggleBtn(child2.id)}}>
+                    {toggles[child2.id]?"+":"-"}
+                  </button>
+                  }
+                  {child2.name}
+                  {child2.children && 
+                    child2.children.map((child3)=>{return(
+                    <ul>
+                      <li hidden={toggles[parents[child3.id]]} data-test={"category-"+child3.id}>{child3.name}</li>
+                    </ul>
+                  )}
+                  )}
+                  </li>
+              )}
+              )}
+              </li>
+            )}
+            )}
+            </ul>
+          }
+        </li>
+        )
+        }
+      )}
+      </ul>
+    </div>
+    ]
+  }
+
+
   return (
     <div>
+      {console.log(testarr)}
+      {testarr}
       <h2>Category tree</h2>
       <ul className="root" data-test="root">
         <li data-test="category-1">
