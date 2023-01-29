ddda79609b3a7a66df534deb9b1e73353f10bb53
combsbt <combsbt@vcu.edu>
2023-01-29 17:30:11 -0500

    save

--- src/tests/TEST_2_9.js
+++ src/tests/TEST_2_9.js
@@ -18,6 +18,8 @@ export default function TEST_2_9() {
     }
   }
 
+  console.log("render")
+
   let jsxArray = []
   for(let i=0; i < columnList.length; i++){
     jsxArray.push(<div key={columnList[i]} className="cell header">{columnList[i]}</div>)
@@ -27,8 +29,12 @@ export default function TEST_2_9() {
     jsxArray2.push(<div key={"header"+(i+1)} className="cell header">{i+1}</div>)
     for(let j=0; j < columnList.length; j++){
       jsxArray2.push(   
-        <div key={columnList[j]+(i+1)} className="cell" data-test={columnList[j]+(i+1)}>
-          <input id={columnList[j]+(i+1)} type="text" value={cells[columnList[j]+(i+1)]} 
+        <div key={columnList[j]+(i+1)} className="cell" data-test={columnList[j]+(i+1)} 
+        onClick={()=>{
+          document.getElementById(columnList[j]+(i+1)).hidden=false;
+          document.getElementById(columnList[j]+(i+1)).focus();
+        }}  >
+          <input hidden={true} id={columnList[j]+(i+1)} type="text" value={cells[columnList[j]+(i+1)]} 
           onKeyDown={(e)=>e.key==="Enter"&&handleCalc(columnList[j]+(i+1))}
           onBlur={()=>handleCalc(columnList[j]+(i+1))} 
           onChange={(e)=>{handleChange([e.target.id,e.target.value])}}/>
@@ -44,9 +50,30 @@ export default function TEST_2_9() {
   }
 
   function handleCalc(cell){
-    if(cells[cell]!==undefined && cells[cell].substring(0,1)==="="){
+    
+    let cellList = []
+    for(let i=0; i < ROWS; i++){
+      for(let j=0; j < columnList.length; j++){
+        cellList.push(columnList[j]+(i+1))
+      }
+    }
+
+    if(cells[cell]!==""&&cells[cell]!==undefined && cells[cell].substring(0,1)==="="){
+      let newCell = cells[cell].substring(1);
+      console.log(cellList)
+      cellList.forEach(cell=>{
+        console.log(cell)
+        if(newCell.includes(cell)){
+          console.log(true)
+          console.log(newCell.indexOf(cell))
+          newCell = newCell.replace(cell,cells[cell])
+          console.log(newCell)
+        }  
+      })
+      
+
       try{
-        setCells({...cells,[cell]:eval(cells[cell].substring(1)).toString()})
+        setCells({...cells,[cell]:eval(newCell).toString()})
       } catch(error){
         setCells({...cells,[cell]:["$ERR"]})
       }
