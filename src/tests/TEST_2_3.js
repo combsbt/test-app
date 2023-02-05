fecdf64b0540b222b700087b2237050bda8565d2
combsbt <combsbt@vcu.edu>
2023-02-05 01:05:02 -0500

    this is stupid, the tests work sometimes and fail others with no changes

--- src/tests/TEST_2_3.js
+++ src/tests/TEST_2_3.js
@@ -2,15 +2,15 @@ import { React, useState, useEffect } from 'react';
 
 export default function TEST_2_3() {
   const [ratio, setRatio] = useState(null);
-  const [file, setFile] = useState(null);
+  console.log('render')
 
-  function handleLoad(){
-    let width = document.getElementById("image").naturalWidth;
-    let height = document.getElementById("image").naturalHeight;
-    let gcdImg = gcd(width,height);
-    if(ratio !== width/gcdImg+"x"+height/gcdImg ){
-    setRatio(width/gcdImg+"x"+height/gcdImg)}
-  }
+  // function handleLoad(){
+  //   let width = document.getElementById("image").naturalWidth;
+  //   let height = document.getElementById("image").naturalHeight;
+  //   let gcdImg = gcd(width,height);
+  //   if(ratio !== width/gcdImg+"x"+height/gcdImg ){
+  //   setRatio(width/gcdImg+"x"+height/gcdImg)}
+  // }
 
   function gcd(a, b) {
     if (b) {
@@ -20,16 +20,32 @@ export default function TEST_2_3() {
     }
   }
 
+  const myImage = new Image(200,300);
+  myImage.onload = function() {
+    let width = myImage.naturalWidth;
+    let height = myImage.naturalHeight;
+    let gcdImg = gcd(width,height);
+    console.log(width)
+    console.log(height)
+    setRatio(width/gcdImg+"x"+height/gcdImg)
+  }
+
+  function findRatio(target) {
+    myImage.src =  URL.createObjectURL(target);
+  }
+
   return (
     <div>
       <h2>Image size ratio</h2>
       <input type="file" data-test="file-input" onChange={(e)=>{
-        setFile(URL.createObjectURL(e.target.files[0]))
+        findRatio(e.target.files[0])
       }}/>
       <div className="result" data-test="result">
         {ratio}
       </div>
-      <img style={{display:"none"}} id="image" src={file && file} onLoad={ handleLoad }/>
+      
     </div>
   );
 }
+
+//<img style={{display:"none"}} id="image" src={file && file} onLoad={ handleLoad }/>
